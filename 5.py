e81774b881ce8cf66fa12c5f553828c42b80bfe9
kolesroma <72313928+kolesroma@users.noreply.github.com>
2020-12-29 19:04:47 +0200

    Smallest multiple

---
+++ 5.py
@@ -0,0 +1,12 @@
+# Smallest multiple
+
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+for i in range(1,10**10**10):
+    if i%2==0 and i%3==0 and i%4==0 and i%5==0 and i%6==0 and i%7==0 and i%8==0 and i%9==0 and i%10==0 and i%11==0 and i%12==0 and i%13==0 and i%14==0 and i%15==0 and i%16==0 and i%17==0 and i%18==0 and i%19==0 and i%20==0:
+        print(i)
+
+               
+print('end')
+            
